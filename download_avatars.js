7ffbddd44e8a171cf056fbe2076b2d8619381ace
Dave <dave.samson91@gmail.com>
2018-09-03 23:51:43 +0000

    Authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,4 +1,23 @@
 var request = require('request');
-
-
+var secrets = require(/'secrets');
 console.log('Welcome to GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+    var options = {
+        url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    header {
+        "User-Agent" : "Request"
+        "Authorization" : GITHUB_TOKEN;
+        }
+    };
+    request(url, function(err, res, body){
+        cb(err, body);
+        });
+  }
+
+
+getRepoContributors("jquery", "jquery", function (err, results) {
+    console.log("Error: ", err);
+    console.log("Results: ", results);
+  });
+

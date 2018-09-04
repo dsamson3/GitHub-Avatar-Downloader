5a971e9e6b6cfdea9e432a2413a7281267863a0d
Dave <dave.samson91@gmail.com>
2018-09-04 00:50:01 +0000

    Return Urls

--- download_avatars.js
+++ download_avatars.js
@@ -1,23 +1,33 @@
 var request = require('request');
-var secrets = require(/'secrets');
+var getToken = require('./secrets');
 console.log('Welcome to GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
-    header {
-        "User-Agent" : "Request"
-        "Authorization" : GITHUB_TOKEN;
+    headers: {
+        'User-Agent' : 'request',
+        'Authorization' : getToken.GITHUB_TOKEN + ' OAUTH-TOKEN',
         }
     };
-    request(url, function(err, res, body){
+    request(options, function(err, res, body){
         cb(err, body);
         });
   }
-
+function getAvatarURL (err, body){
+    if(err) {throw err;}
+    else {
+        cosole.log(JSON.parse(body));
+    } 
+} 
 
 getRepoContributors("jquery", "jquery", function (err, results) {
     console.log("Error: ", err);
-    console.log("Results: ", results);
-  });
+    arrayOfContributorsObj = JSON.parse(results)
+    for(var i = 0; i < arrayOfContributorsObj.length; i++)
+    {
+        console.log(arrayOfContributorsObj[i].avatar_url);
+    }
+    
+   });
 
